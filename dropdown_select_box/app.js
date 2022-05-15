22c8e0c77914816d542072dd0877f8810ff0e83a
sabit1997 <yejiii1997@gmail.com>
2022-05-15 22:17:56 +0900

    list 선택 기능 추가

--- dropdown_select_box/app.js
+++ dropdown_select_box/app.js
@@ -7,3 +7,22 @@ selectBtn.addEventListener("click", () => {
   selectMenu.classList.toggle("active");
   selectBtn.classList.toggle("active");
 });
+
+// 선택 기능
+//  나중에 더 간단하게 짜보기!
+let selectItem = document.querySelectorAll(".list-item");
+selectItem[0].addEventListener("click", () => {
+  selectBtn.textContent = selectItem[0].textContent;
+});
+selectItem[1].addEventListener("click", () => {
+  selectBtn.textContent = selectItem[1].textContent;
+});
+selectItem[2].addEventListener("click", () => {
+  selectBtn.textContent = selectItem[2].textContent;
+});
+selectItem[3].addEventListener("click", () => {
+  selectBtn.textContent = selectItem[3].textContent;
+});
+selectItem[4].addEventListener("click", () => {
+  selectBtn.textContent = selectItem[4].textContent;
+});
