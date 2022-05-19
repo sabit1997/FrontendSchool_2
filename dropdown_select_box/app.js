5fb5511d81df8d44d0f17e6e52bb3b782e442d18
sabit1997 <yejiii1997@gmail.com>
2022-05-19 22:37:04 +0900

    선택기능 수정

--- dropdown_select_box/app.js
+++ dropdown_select_box/app.js
@@ -2,27 +2,15 @@
 
 const selectBtn = document.querySelector(".favorite-lang");
 const selectMenu = document.querySelector(".lang-list");
+const selectItem = selectMenu.querySelectorAll("li");
 
 selectBtn.addEventListener("click", () => {
   selectMenu.classList.toggle("active");
   selectBtn.classList.toggle("active");
 });
 
-// 선택 기능
-//  나중에 더 간단하게 짜보기!
-let selectItem = document.querySelectorAll(".list-item");
-selectItem[0].addEventListener("click", () => {
-  selectBtn.textContent = selectItem[0].textContent;
-});
-selectItem[1].addEventListener("click", () => {
-  selectBtn.textContent = selectItem[1].textContent;
-});
-selectItem[2].addEventListener("click", () => {
-  selectBtn.textContent = selectItem[2].textContent;
-});
-selectItem[3].addEventListener("click", () => {
-  selectBtn.textContent = selectItem[3].textContent;
-});
-selectItem[4].addEventListener("click", () => {
-  selectBtn.textContent = selectItem[4].textContent;
+selectItem.forEach((el, i) => {
+  selectItem[i].addEventListener("click", () => {
+    selectBtn.textContent = selectItem[i].textContent;
+  });
 });
