b11db3b497a3e74be220c477cf479288c5c4a6eb
sabit1997 <yejiii1997@gmail.com>
2022-06-22 23:27:42 +0900

    title, content, time 나타내기

--- 메모장/app.js
+++ 메모장/app.js
@@ -3,13 +3,13 @@ const inputContent = document.querySelector('#content');
 const uploadButton = document.querySelector('.upload-button');
 const saveText = document.querySelector('.save-text');
 
-const titlesArr = [];
-const contentsArr = [];
-const timesArr = [];
+let titlesArr = [];
+let contentsArr = [];
+let timesArr = [];
 
 function saveArr() {
-  const titles = JSON.stringify(inputTitle.value);
-  const contents = JSON.stringify(inputContent.value);
+  const titles = inputTitle.value;
+  const contents = inputContent.value;
   const now = new Date();
   const times = JSON.stringify(now.toLocaleString('ko-KR'));
   titlesArr.push(titles);
@@ -18,9 +18,14 @@ function saveArr() {
 }
 
 function saveArticle() {
-  window.localStorage.setItem('title', titlesArr);
-  window.localStorage.setItem('contents', contentsArr);
-  window.localStorage.setItem('times', timesArr);
+  window.localStorage.setItem('title', JSON.stringify(titlesArr));
+  window.localStorage.setItem('contents', JSON.stringify(contentsArr));
+  window.localStorage.setItem('times', JSON.stringify(timesArr));
+}
+
+function deleteArticle(event) {
+  li = event.target.parentElements;
+  li.remove();
 }
 
 uploadButton.addEventListener('click', () => {
@@ -34,13 +39,35 @@ uploadButton.addEventListener('click', () => {
   } else {
     saveArr();
     saveArticle();
+    // // 나타내기
+    const parseTitle = JSON.parse(window.localStorage.getItem('title'));
+    const parseContents = JSON.parse(window.localStorage.getItem('contents'));
+    const parseTimes = JSON.parse(window.localStorage.getItem('times'));
+    // 엘레먼트 만들기
     const li = document.createElement('li');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
     const span = document.createElement('span');
+    const button = document.createElement('button');
+    button.innerText = 'X';
+    button.addEventListener('click', deleteArticle);
+    parseTitle.forEach((element) => {
+      h3.textContent = element;
+    });
+    parseContents.forEach((element) => {
+      p.textContent = element;
+    });
+    parseTimes.forEach((element) => {
+      span.textContent = element;
+    });
+    titlesArr = parseTitle;
+    contentsArr = parseContents;
+    timesArr = parseTimes;
+    // 위치 시키기
     li.appendChild(h3);
     li.appendChild(p);
     li.appendChild(span);
+    li.appendChild(button);
     saveText.appendChild(li);
     inputTitle.value = '';
     inputContent.value = '';
